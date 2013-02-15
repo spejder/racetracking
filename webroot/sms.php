120578f7fb58f94340d09bd837316b324281191b
Mads H. Danquah <mads@danquah.dk>
2013-02-15 22:39:09 +0100

    Cleanup and comments

--- webroot/sms.php
+++ webroot/sms.php
@@ -1,21 +1,28 @@
 <?php
-require_once('db.inc.php');
+/**
+ * @file
+ * Interface to the sms system.
+ */
 
-$mysqli = new mysqli("localhost", DBUSER, DBPASS, DB);
-
-/* check connection */
-if (mysqli_connect_errno()) {
-  printf("Connect failed: %s\n", mysqli_connect_error());
-  exit();
-}
+require_once 'db.inc.php';
 
 report($_REQUEST['message'], $mysqli);
 
 $mysqli->close();
 
-
+/**
+ * Processes the input from the sms gateway.
+ *
+ * @param string $input
+ *   String sent in via SMS.
+ *
+ * @param object $mysqli
+ *   DB connection.
+ */
 function report($input, $mysqli) {
-  preg_match("/spejder ([a-z0-9]+) (p[0-9]{1,2}) (h[0-9]{1,2}) ([0-9]{3,4}) ([0-9]{3,4})( x)?/i", $input, $matches);
+  // TODO support matching both with an without post specification,
+  // if spec is missing, determine post via number associated to the post.
+  preg_match("/spejder ([a-z0-9]+) (p[0-9]{1,2}) (h[0-9]{1,2}) ([0-9]{3,4}) ([0-9]{3,4})?/i", $input, $matches);
 
   $keyword = $matches[1];
   $post_smskey = $matches[2];
@@ -23,11 +30,6 @@ function report($input, $mysqli) {
 
   $arrival_time = $matches[4];
   $departure_time = $matches[5];
-
-  $straf = FALSE;
-  if (strtolower(trim($matches[6])) == "x") {
-    $straf = TRUE;
-  }
 
   // Get teamid.
   $teamid = get_team_id($team_smskey, $mysqli);
@@ -40,12 +42,15 @@ function report($input, $mysqli) {
 
   if ($teamid && $postid) {
     if (strlen($arrival_time) == 3) {
+      // Eg 01:00 as 100.
       $arrival_time = mktime(substr($arrival_time, 0, 1), substr($arrival_time, 1), 0, $month, $day, $year);
     }
     else {
+      // Eg 01:00 as 0100 or 23:00 as 2300
       $arrival_time = mktime(substr($arrival_time, 0, 2), substr($arrival_time, 2), 0, $month, $day, $year);
     }
 
+    // Same handling as above.
     if (strlen($departure_time) == 3) {
       $departure_time = mktime(substr($departure_time, 0, 1), substr($departure_time, 1), 0, $month, $day, $year);
     }
@@ -58,9 +63,11 @@ function report($input, $mysqli) {
       $arrival_time -= 60 * 60 * 24;
     }
 
+    // Unix-timestamps created, now format them.
     $departure_time = date("Y-m-d H:i:00", $departure_time);
     $arrival_time = date("Y-m-d H:i:00", $arrival_time);
 
+    // And insert the visit into the database.
     if ($stmt = $mysqli->prepare("INSERT INTO visit (teamid, postid, type, time) VALUES (?,?,?,?)")) {
       $type = "a";
       $stmt->bind_param("iiss", $teamid, $postid, $type, $arrival_time);
@@ -71,27 +78,30 @@ function report($input, $mysqli) {
       $stmt->execute();
 
       $stmt->close();
-      $stmt = $mysqli->prepare("INSERT INTO visit (teamid, postid, type, time) VALUES (?,?,?,NOW())");
-      if ($straf) {
-        $type = "x";
-        $stmt->bind_param("iis", $teamid, $postid, $type);
-        $stmt->execute();
-      }
-      $stmt->close();
 
       print ("Modtaget, hold $teamid ankommet til post $postid kl $arrival_time afgang kl $departure_time");
-      if ($straf) {
-        print (" (+strafpost)");
-      }
     }
   }
   else {
+    // TODO: Describe the usage without post name
     print ("Ugyldig kommando, syntax: spejder $keyword p<postnr> h<holdnr> <ankomst> <afgang> [x]\n");
     print ("Eksempel 1: spejder $keyword p1 h2 2200 2300\n");
-    print ("Eksempel 2: spejder $keyword p3 h4 2300 0200 x");
+    print ("Eksempel 2: spejder $keyword h4 2300 0200");
   }
 }
 
+/**
+ * Git a teams id via its smskey.
+ *
+ * @param string $team_smskey
+ *   The short smskey identifier.
+ *
+ * @param object $mysqli
+ *   An initialised database connection.
+ *
+ * @return int
+ *   The id.
+ */
 function get_team_id($team_smskey, $mysqli) {
   if ($stmt = $mysqli->prepare("SELECT teamid FROM team where smskey = ?")) {
     // Execute query.
@@ -116,6 +126,18 @@ function get_team_id($team_smskey, $mysqli) {
   }
 }
 
+/**
+ * Git a posts id via its smskey.
+ *
+ * @param string $post_smskey
+ *   The short smskey identifier.
+ *
+ * @param object $mysqli
+ *   An initialised database connection.
+ *
+ * @return int
+ *   The id.
+ */
 function get_post_id($post_smskey, $mysqli) {
   if ($stmt = $mysqli->prepare("SELECT postid FROM post where smskey = ?")) {
     // Execute query.
