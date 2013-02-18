fe270a4984df48e13b771e92531224d2f50ef8b3
Mads H. Danquah <mads@danquah.dk>
2013-02-18 21:21:24 +0100

    Update db structure.

--- db-clean.sql
+++ db-clean.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.30, for apple-darwin9.5.0 (i386)
+-- MySQL dump 10.14  Distrib 5.3.8-MariaDB, for apple-darwin12.1.0 (i386)
 --
--- Host: localhost    Database: ccmrsms
+-- Host: localhost    Database: racetracking
 -- ------------------------------------------------------
--- Server version	5.1.30
+-- Server version	5.3.8-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -20,16 +20,17 @@
 --
 
 DROP TABLE IF EXISTS `post`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `post` (
   `postid` int(11) NOT NULL AUTO_INCREMENT,
   `name` tinytext,
-  `description` text,
   `smskey` tinytext,
+  `canceled` tinyint(4) NOT NULL DEFAULT '0',
+  `phonenumber` tinytext,
   PRIMARY KEY (`postid`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `post`
@@ -37,7 +38,7 @@ SET character_set_client = @saved_cs_client;
 
 LOCK TABLES `post` WRITE;
 /*!40000 ALTER TABLE `post` DISABLE KEYS */;
-INSERT INTO `post` VALUES (1,'Post 01',NULL,'p1'),(2,'Post 02',NULL,'p2'),(3,'Post 03',NULL,'p3'),(4,'Post 04',NULL,'p4'),(5,'Post 05',NULL,'p5'),(6,'Post 06',NULL,'p6'),(7,'Post 07',NULL,'p7'),(8,'Post 08',NULL,'p8'),(9,'Post 09',NULL,'p9'),(10,'Post 10',NULL,'p10'),(11,'Post 11',NULL,'p11'),(12,'Post 12',NULL,'p12'),(13,'Post 13',NULL,'p13'),(14,'Post 14',NULL,'p14'),(15,'Post 15',NULL,'p15');
+INSERT INTO `post` VALUES (1,'Post 01','p1',0,''),(2,'Post 02','p1',0,NULL);
 /*!40000 ALTER TABLE `post` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -46,15 +47,16 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `team`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `team` (
   `teamid` int(11) NOT NULL AUTO_INCREMENT,
   `name` tinytext,
   `smskey` tinytext,
+  `canceled` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`teamid`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `team`
@@ -62,7 +64,7 @@ SET character_set_client = @saved_cs_client;
 
 LOCK TABLES `team` WRITE;
 /*!40000 ALTER TABLE `team` DISABLE KEYS */;
-INSERT INTO `team` VALUES (1,'Hold 01','h1'),(2,'Hold 02','h2'),(3,'Hold 03','h3'),(4,'Hold 04','h4'),(5,'Hold 05','h5'),(6,'Hold 06','h6'),(7,'Hold 07','h7'),(8,'Hold 08','h8'),(9,'Hold 09','h9'),(10,'Hold 10','h10'),(11,'Hold 11','h11');
+INSERT INTO `team` VALUES (1,'Hold 1310','h1',0),(2,'Hold 1311','h1',0);
 /*!40000 ALTER TABLE `team` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -71,15 +73,15 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `visit`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `visit` (
   `teamid` int(11) NOT NULL,
   `postid` int(11) NOT NULL,
   `type` varchar(1) DEFAULT NULL,
   `time` datetime DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `visit`
@@ -99,4 +101,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2009-01-15 22:28:33
+-- Dump completed on 2013-02-18 21:20:05
--- db-structure.sql
+++ db-structure.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.30, for apple-darwin9.5.0 (i386)
+-- MySQL dump 10.14  Distrib 5.3.8-MariaDB, for apple-darwin12.1.0 (i386)
 --
--- Host: localhost    Database: ccmrsms
+-- Host: localhost    Database: racetracking
 -- ------------------------------------------------------
--- Server version       5.1.30
+-- Server version	5.3.8-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -20,44 +20,48 @@
 --
 
 DROP TABLE IF EXISTS `post`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `post` (
   `postid` int(11) NOT NULL AUTO_INCREMENT,
   `name` tinytext,
-  `description` text,
+  `smskey` tinytext,
+  `canceled` tinyint(4) NOT NULL DEFAULT '0',
+  `phonenumber` tinytext,
   PRIMARY KEY (`postid`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `team`
 --
 
 DROP TABLE IF EXISTS `team`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `team` (
   `teamid` int(11) NOT NULL AUTO_INCREMENT,
   `name` tinytext,
+  `smskey` tinytext,
+  `canceled` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`teamid`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
+) ENGINE=MyISAM AUTO_INCREMENT=48 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `visit`
 --
 
 DROP TABLE IF EXISTS `visit`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `visit` (
   `teamid` int(11) NOT NULL,
   `postid` int(11) NOT NULL,
   `type` varchar(1) DEFAULT NULL,
   `time` datetime DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -68,4 +72,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2009-01-11 19:59:54
+-- Dump completed on 2013-02-18 21:16:48
